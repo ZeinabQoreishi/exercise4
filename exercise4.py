728cb38a013cdfe80aad95a92c830c4e39926cd9
ZeinabQoreishi <zeinabqoreishi97@gmail.com>
2025-03-10 13:37:03 -0700

    Add files via upload

---
+++ exercise4.py
@@ -0,0 +1,21 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+delta_e = np.array(input(' delta_e = '))
+print(' delta_e = ', delta_e)
+
+teta =odeit( )
+
+
+
+
+M_alpha = -8.790 
+M_q = -2.075
+M_dee = -11.87
+
+
+time = np.linspace(0,0.01)
+
+
+
